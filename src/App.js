bd5c97727ee2e85496048f4ddd81e60ba8b414d5
Joe Rocha <Joe.Rocha@me.com>
2019-02-14 19:10:49 -0800

    Working Table with binded data

--- src/App.js
+++ src/App.js
@@ -1,27 +1,35 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import ChooseAthlete from './ChooseAthlete';
 import './App.css';
 
 class App extends Component {
+
+
   render() {
+    const athletes = [
+      {
+          'name': 'Anderson Silva',
+          'job': 'Middleweight'
+      },
+      {
+          'name': 'Mac',
+          'job': 'Bouncer'
+      },
+      {
+          'name': 'Dee',
+          'job': 'Aspring actress'
+      },
+      {
+          'name': 'Dennis',
+          'job': 'Bartender'
+      }
+  ];
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="container">
+        <ChooseAthlete athleteData={athletes} />
       </div>
-    );
+    )
   }
 }
 
